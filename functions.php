366ac96957b2c561abb9da9a16d2a777904207eb
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-07 22:38:59 +0200

    moved logic to functions.php, base exercise finished

--- functions.php
+++ functions.php
@@ -1,2 +1,28 @@
 <?php
-echo "Prova";
+$invalid = 'Inserisci una lunghezza valida.';
+$randomPassword = '';
+
+function generatePassword()
+{
+    $availableChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789!?/';
+    $randomPassword = '';
+    for ($i = 0; $i < $_GET['passwordLength']; $i++) {
+        $randomPassword .= $availableChars[rand(0, strlen($availableChars))];
+    }
+    return $randomPassword;
+}
+
+if (isset($_GET['passwordLength'])) {
+    $passwordLength = $_GET['passwordLength'];
+    if (!is_numeric($passwordLength) && ($passwordLength <= 6)) {
+        $invalid = 'Nessuna password generata.';
+    }
+}
+
+
+
+if (is_numeric($passwordLength) && ($passwordLength > 6)) {
+    echo $randomPassword = generatePassword($passwordLength);
+} else {
+    echo $invalid;
+}
--- index.php
+++ index.php
@@ -2,31 +2,8 @@
 include __DIR__ . '/functions.php';
 
 
-$invalid = 'Inserisci una lunghezza valida.';
-$randomPassword = '';
 
-if (isset($_GET['passwordLength'])) {
-    $passwordLength = $_GET['passwordLength'];
-    if (!is_numeric($passwordLength) && ($passwordLength <= 6)) {
-        $invalid = 'Nessuna password generata.';
-    }
-}
 
-function generatePassword()
-{
-    $availableChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789!?/';
-    $randomPassword = '';
-    for ($i = 0; $i < $_GET['passwordLength']; $i++) {
-        $randomPassword .= $availableChars[rand(0, strlen($availableChars))];
-    }
-    return $randomPassword;
-}
-
-if (is_numeric($passwordLength) && ($passwordLength > 6)) {
-    echo $randomPassword = generatePassword($passwordLength);
-} else {
-    echo $invalid;
-}
 
 ?>
 <!DOCTYPE html>
